a1881b2ba4340f61bd4e1c2d2592827c6c3c339b
Alex Miroshnichenko <pocanchik88@mail.ru>
2018-03-24 23:46:40 +0400

    *devise(sign_in,sign_out)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,35 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :integer          not null, primary key
+#  name                   :string
+#  user_mail              :string
+#  user_phone             :string
+#  user_address           :text
+#  user_profession        :string
+#  user_exp               :string
+#  user_info              :text
+#  user_vk                :string
+#  user_inst              :string
+#  user_fb                :string
+#  user_tvitter           :string
+#  user_telegram          :string
+#  user_skype             :string
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  sign_in_count          :integer          default(0), not null
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :inet
+#  last_sign_in_ip        :inet
+#
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
 
   devise_scope :user do
-    get "/sign_in" => "devise/sessions#new" # custom path to login/sign_in
+
     get "/sign_up" => "devise/registrations#new", as: "new_user_registration" # custom path to sign_up/registration
     get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
     put 'users' => 'devise/registrations#update', :as => 'user_registration'
+    get 'sign_in', to: 'devise/sessions#new'
+    post 'sign_in', to: 'devise/sessions#create'
+    delete 'sign_out', to: 'devise/sessions#destroy'
+
   end
 
   devise_for :users
